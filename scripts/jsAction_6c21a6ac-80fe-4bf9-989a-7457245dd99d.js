090a6fc8cd1e7f337520aa98f921086dea962b97
Brad Stoner <Brad Stoner>
2016-08-02 14:55:47 -0700

    origional upload

---
+++ scripts/jsAction_6c21a6ac-80fe-4bf9-989a-7457245dd99d.js
@@ -0,0 +1,19 @@
+﻿// Javascript skeleton.
+// Edit and adapt to your needs.
+// The documentation of the NeoLoad Javascript API
+// is available in the appendix of the documentation.
+
+// Get variable value from VariableManager
+var computedValue = context.variableManager.getValue("null");
+//if (myVar==null) {
+//        context.fail("Variable 'myVar' not found");
+//}
+
+// Do some computation using the methods
+// you defined in the JS Library
+//var computedValue = myLibraryFunction(myVar);
+//var computedValue = 1;
+logger.debug("ComputedValue="+computedValue);
+
+// Inject the computed value in a runtime variable
+context.variableManager.setValue("computedVar",computedValue);
---
+++ scripts/jsAction_ca3d3705-d5ea-4686-b629-d4c628bf7cbd.js
@@ -0,0 +1,14 @@
+﻿// Javascript skeleton.
+// Edit and adapt to your needs.
+// The documentation of the NeoLoad Javascript API
+// is available in the appendix of the documentation.
+
+// Get variable value from VariableManager
+var f_name = context.variableManager.getValue("f_name");
+var l_name = context.variableManager.getValue("l_name");
+
+
+// Do some computation using the methods
+// you defined in the JS Library
+var login = f_name+ "@" + l_name + ".com";
+logger.debug("Login="+login);
